189350af3f24171d2d87a401e9c8ad189feba692
Paul Kennedy <pkkenne.dev@gmail.com>
2026-05-04 09:10:44 -0400

    testing

--- src/qtcore/vtk_utils.py
+++ src/qtcore/vtk_utils.py
@@ -2,10 +2,7 @@ from vtkmodules.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 
 from pycore.platform import IS_MACOS
 
-if IS_MACOS:
-
-    class SafeVTKWidget(QVTKRenderWindowInteractor):
+class SafeVTKWidget(QVTKRenderWindowInteractor):
         def paintEvent(self, ev):
             pass
-else:
-    SafeVTKWidget = QVTKRenderWindowInteractor
+
